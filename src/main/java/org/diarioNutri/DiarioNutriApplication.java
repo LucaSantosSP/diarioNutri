0e06ea241875f6873e37f3d5079db97680f7c68d
LucaSantosSP <lucas-santos20.17@hotmail.com>
2023-08-03 00:08:43 -0300

    fix: Configurado ambientes

--- src/main/java/org/diarioNutri/DiarioNutriApplication.java
+++ src/main/java/org/diarioNutri/DiarioNutriApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DiarioNutriApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
-    private String applicationName;
+    public String applicationName(){
+        return "Diario Nutricional";
+    }
 
     @GetMapping("/hello")
     public String helloWorld(){
-        return applicationName;
+        return applicationName();
     }
     public static void main(String[] args) {
         SpringApplication.run(DiarioNutriApplication.class, args);
--- src/main/java/org/diarioNutri/MyConfiguration.java
+++ src/main/java/org/diarioNutri/MyConfiguration.java
@@ -1,13 +1,23 @@
 package org.diarioNutri;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MyConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Diario Nutricional";
+    @Value("${application.name}")
+    private String ambiente;
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("---------------------------Rodando " + ambiente + "!---------------------------");
+        };
     }
+
+
 }
